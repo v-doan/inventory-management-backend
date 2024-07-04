74e680dc814f57a75970553549a8848ee546023b
v-doan <vyndoan@gmail.com>
2024-07-04 18:09:54 -0400

    more cors until it works

--- src/main/java/com/example/inventory_management/config/GlobalCORSConfiguration.java
+++ src/main/java/com/example/inventory_management/config/GlobalCORSConfiguration.java
@@ -2,23 +2,24 @@ package com.example.inventory_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class GlobalCORSConfiguration {
 
     @Bean
-    public WebMvcConfigurer globalCorsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://inventory-management-backend-f36ec1d11345.herokuapp.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://inventory-management-backend-f36ec1d11345.herokuapp.com"); // Replace with your frontend domain
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
