b86e2b6b06c1c2afbea9aa21c10e0a23c4633efe
v-doan <vyndoan@gmail.com>
2024-07-04 16:51:47 -0400

    added error view

---
+++ src/main/java/com/example/inventory_management/controller/CustomErrorController.java
@@ -0,0 +1,17 @@
+package com.example.inventory_management.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    @ResponseBody
+    public String handleError() {
+        // Custom error handling logic
+        return "An error occurred!";
+    }
+}
